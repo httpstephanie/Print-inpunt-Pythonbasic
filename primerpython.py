0e68778fe525367e0727b489eb43e02dee291145
estefania <estefirbz@gmail.com>
2022-09-17 19:02:49 -0500

    Initial Commit

---
+++ primerpython.py
@@ -0,0 +1,4 @@
+#print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+#print("Yo estoy estudiando" + input("¿Qué estás estudiando?"))
+# Ejercicio sin string adicional print(input("¿Qué estás estudiando?"))
+print("Tus respuestas\nson '" + input("Que pais te gustaria visitar?") + " y " + input("Cual es tu color favorito?: ") + "'\nGrandioso!" )
